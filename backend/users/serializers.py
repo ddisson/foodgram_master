fd2ec045bbb03f3a8f84372e5110b4f86178ec79
Dmitry <ddisson@yandex.ru>
2023-11-10 23:06:44 +0800

    2250commit

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-from rest_framework.validators import UniqueTogetherValidator
 from django.contrib.auth.password_validation import validate_password
 from rest_framework.fields import CurrentUserDefault
 
